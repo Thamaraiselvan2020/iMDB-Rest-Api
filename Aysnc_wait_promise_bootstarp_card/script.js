9b71580739a60a5fdbda85db7388d87761472252
Thamaraiselvan Balakrishnan <thamarai114nkl@gmail.com>
2020-09-13 18:15:38 +0530

    Add files via upload

---
+++ Aysnc_wait_promise_bootstarp_card/script.js
@@ -0,0 +1,174 @@
+
+loadCountries = () => {
+    let url = 'https://restcountries.eu/rest/v2/all';
+    let countryData = document.getElementById('country-data');
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            getRestCountries(data, countryData, 'getWeatherFetchData');
+        })
+        .catch(err => {
+            console.log("Error:", err);
+        });
+}
+
+loadCountries();
+
+getRestCountries = (param, countryData, getWeatherData) => {
+    let tags = '';
+    let count = 1;
+    let divRow = document.createElement('div');
+    console.log(param);
+    for (let i = 0; i < param.length; i++) {
+
+        divRow.setAttribute('class', 'row');
+        let divCol = document.createElement('div');
+        divCol.setAttribute('class', 'col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 mt-3');
+        tags = `
+            <div class="card card-custom-css">
+                <div class="card-header">
+                   ${count}). ${param[i].name}
+                </div>
+                <img src="${param[i].flag}" id="${param[i].alpha2Code}" class="card-img-top cust-card-img" alt="${param[i].name}">
+                <div class="card-body card-body-custom-css">
+                    <p class="card-text">
+                        Capital: <span class="badge badge-success"> ${param[i].capital} </span><br>
+                        Country Codes: <span class="badge "> ${param[i].alpha2Code}, ${param[i].alpha3Code}</span><br>
+                        Region: <span class="badge "> ${param[i].region}</span>
+                    </p>
+                </div>
+                <div class="card-footer text-muted text-center">
+                <button class="btn btn-sm btn-primary" data-toggle="alert" id="whoData" onclick="${getWeatherData}('${param[i].capital}','${param[i].alpha2Code}','${param[i].name}')">View Current Weather</button>
+            </div>
+            </div>
+            `;
+        count++;
+        divCol.innerHTML = tags;
+        divRow.appendChild(divCol);
+        countryData.appendChild(divRow);
+    }
+}
+
+
+
+function getWeatherFetchData(value, code, country) {
+    var urlValues = '';
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+    var weatherAPIKey = '&appid=c3b4ef31feb237ca322297881c170281';
+    var weatherMetric = '&units=metric';
+    urlValues += weatherURL + value + ',' + code + weatherAPIKey + weatherMetric
+
+    fetch(urlValues)
+        .then(res => res.json())
+        .then((data) => {
+            alert('You selected Country: ' + country + ' with capital ' + data.name + ' and Temperature(in celsius) ' + data.main.temp);
+        }).catch((err) => {
+            alert("Couldn't find data for the selected country, Try again!");
+            // console.log("Error:", err)
+        });
+
+}
+
+let loadAsyncAwaitFunction = async() => {
+
+    let url = 'https://restcountries.eu/rest/v2/all';
+    let countryData = document.getElementById('country-data-await');
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        getRestCountries(data, countryData, 'getWeatherAwaitData');
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
+
+
+async function getWeatherAwaitData(value, code, country) {
+    var urlValues = '';
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+    var weatherAPIKey = '&appid=c3b4ef31feb237ca322297881c170281';
+    var weatherMetric = '&units=metric';
+    urlValues += weatherURL + value + ',' + code + weatherAPIKey + weatherMetric
+
+    try {
+        let response = await fetch(urlValues);
+        let data = await response.json();
+        alert('You selected Country: ' + country + ' with capital ' + data.name + ' and Temperature(in celsius) ' + data.main.temp);
+    } catch (error) {
+        alert("Couldn't find data for the selected country, Try again!");
+        console.log("Error:", error);
+    }
+
+}
+
+loadAsyncAwaitFunction();
+
+let singleMovieCard = async(imdbID) => {
+    try {
+        let urlSingle = `https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?i=${imdbID}&apikey=e9d1a1c3`
+        let response = await fetch(urlSingle);
+        let data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+let getImdbResults = async() => {
+    try {
+        let searchText = document.getElementById('search').value;
+        console.log(searchText);
+        let url = `https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?s=${searchText}&apikey=e9d1a1c3`
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+
+
+        if (data != null && data.Response != 'False' && data.Search != null) {
+            let movieData = [];
+            for (let i = 0; i < data.Search.length; i++) {
+                let movieDetail = await singleMovieCard(data.Search[i].imdbID);
+                movieData.push(movieDetail);
+            }
+
+            console.log(movieData);
+            let imdbMovieCard = "";
+            movieData.forEach(element => {
+                console.log('inside');
+                imdbMovieCard += `
+                <div class="col-12 col-md-3 imdb-card">
+                
+                <div class="card card-custom-css">
+                <div class="card-header">
+                   ${element.Title}
+                </div>
+                <img src="${element.Poster || ''}" id="imdb-image" class="card-img-top cust-card-img" alt="no results image">
+                <div class="card-body card-body-custom-css">
+                    <p class="card-text">
+                        IMDB RATING: <span class="badge badge-success"> ${element.imdbRating} </span><br>
+                        Language: <span class="badge "> ${element.Language}</span><br>
+                        Genre: <span class="badge "> ${element.Genre}</span>
+                    </p>
+                </div>
+            </div>
+                </div>
+                
+                `;
+            });
+
+
+            document.getElementById('imdb-data').innerHTML = imdbMovieCard;
+        } else {
+
+            document.getElementById('imdb-data').innerHTML = '';
+            throw data.Error;
+        }
+
+    } catch (error) {
+        alert(`Error! No data found! ${error}`)
+        console.log("Error:", error);
+    }
+
+}
